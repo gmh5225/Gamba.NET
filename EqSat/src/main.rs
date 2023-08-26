232995685beafe07758a2106e5d2aca26381fc29
Colton1skees <Colton1skees@gmail.com>
2023-08-26 09:09:21 -0400

    add expression structure

--- EqSat/src/main.rs
+++ EqSat/src/main.rs
@@ -1,3 +1,22 @@
+use egg::*;
+
+define_language! {
+    pub enum Expr {
+        // operations
+        "**" = Pow([Id; 2]),             // (+ a b)
+        "~" = Neg([Id; 1]),              // (~ a)
+        "*" = Mul([Id; 2]),              // * a b)
+        "+" = Add([Id; 2]),              // (+ a b)
+        "&" = And([Id; 2]),              // (& a b)
+        "^" = Xor([Id; 2]),              // (^ a b)
+        "|" = Or([Id; 2]),               // (| a b)
+
+        // Values:
+        Variable(char),                  // (x) 
+        Constant(i64),                   // (int)
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
